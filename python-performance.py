d6631eee271b7b2ec787837e0a0535ffb002b7df
Vernieri <31966345+vernieri@users.noreply.github.com>
2018-08-16 16:26:57 -0300

    Python
    Now its time for Python :)

---
+++ python-performance.py
@@ -0,0 +1,60 @@
+//
+// Pascal-Performance 
+// Version 0.0.1 
+// By: Vernieri
+// 16 August 2018
+// GitHub: github.com/vernieri
+//
+import time
+import random
+
+a = [None] * 99999
+
+def main():
+	t0 = time.time()
+	generator()
+	calc()
+	#print 'hello'
+	t1 = time.time()
+	total = t1-t0
+	print 'Tempo em segundos: %f' %total
+
+def generator():
+	i = 0
+	print 'Lets generate some numbers \n'
+	while i<99999:
+		a[i] = random.randint(1,99999)
+		i += 1
+
+	i = 0	
+	while i<99999:
+		print 'A random Number: %d' %a[i]
+		i += 1
+
+
+def calc():
+	#i,e,n,p,prime
+	prime = 0
+	i = 0
+
+	while i<99999:
+		p = 2
+		n = a[i]
+		e = 0
+
+		while p<n:
+			if((n % p) != 0):
+				e+=1
+
+			p+=1
+
+		if((e+2) == n):
+			#print 'Entrei'
+			prime+=1
+
+		i+=1
+	print 'Here the prime numbers found: %d \n' %prime
+
+main()
+
+//python python-performance.py
